d251b236bf00b2648da938a5530168e2b5fea2af
Moises Borges <moises.borges@mindsea.com>
2019-08-15 13:41:03 -0300

    Fix spacing

--- demo/src/main/java/com/mindsea/TweakManager.kt
+++ demo/src/main/java/com/mindsea/TweakManager.kt
@@ -7,7 +7,7 @@ class TweakManager {
 
     val featureFlagEnabled: Boolean by booleanTweak("login_enabled", "Feature Flags", "Login Enabled", true)
 
-    val animationDuration : Int by intTweak("animation_duration", "Animation Durations", "Fade Animation Duration", 300, 0, 1000, 50)
+    val animationDuration: Int by intTweak("animation_duration", "Animation Durations", "Fade Animation Duration", 300, 0, 1000, 50)
 
     companion object {
         val instance = TweakManager()
