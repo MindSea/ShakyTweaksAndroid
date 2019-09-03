7468a96ce816de6fb03ca2eb25356d7f1cd22a62
Moises Borges <moises.borges@mindsea.com>
2019-09-03 13:39:26 -0300

    Add string tweak ui

--- demo/src/main/java/com/mindsea/TweakManager.kt
+++ demo/src/main/java/com/mindsea/TweakManager.kt
@@ -2,12 +2,15 @@ package com.mindsea
 
 import com.mindsea.shakytweaks.booleanTweak
 import com.mindsea.shakytweaks.intTweak
+import com.mindsea.shakytweaks.stringTweak
 
 class TweakManager {
 
     val featureFlagEnabled: Boolean by booleanTweak("login_enabled", "Feature Flags", "Login Enabled", true)
 
     val animationDuration: Int by intTweak("animation_duration", "Animation Durations", "Fade Animation Duration", 300, 0, 1000, 50)
+
+    val title: String by stringTweak("title", "Strings", "Title of main screen", "Default Title")
 
     companion object {
         val instance = TweakManager()
